699e85dc76cd9e2ece837ef75ffad30a61ed8003
neguse <negcee@gmail.com>
2025-03-11 00:53:44 +0900

    build 09

---
+++ src/GameLibs/InputDevices/src/GameLib/GameLib.h
@@ -0,0 +1,48 @@
+#ifndef INCLUDED_GAMELIB_GAMELIB_H
+#define INCLUDED_GAMELIB_GAMELIB_H
+
+namespace GameLib {
+void halt(const char* filename, int line, const char* message);
+}
+
+// マクロ類
+#define STRONG_ASSERT(exp)                                \
+  ((!!(exp)) || (GameLib::halt(__FILE__, __LINE__, #exp), \
+                 0))  // この行の意味を理解するのは相当難しい
+#define HALT(exp)                            \
+  {                                          \
+    GameLib::halt(__FILE__, __LINE__, #exp); \
+  }
+#define SAFE_DELETE(x) \
+  {                    \
+    delete (x);        \
+    (x) = 0;           \
+  }
+#define SAFE_DELETE_ARRAY(x) \
+  {                          \
+    delete[] (x);            \
+    (x) = 0;                 \
+  }
+#define OPERATOR_DELETE(x) \
+  {                        \
+    operator delete(x);    \
+    (x) = 0;               \
+  }
+
+// デバグとリリースで分岐するもの
+#ifndef NDEBUG
+#define ASSERT(exp) ((!!(exp)) || (GameLib::halt(__FILE__, __LINE__, #exp), 0))
+#define NEW new (__FILE__, __LINE__)
+#define OPERATOR_NEW(x) operator new((x), __FILE__, __LINE__)
+#else  // NDEBUG
+#define NEW new
+#define OPERATOR_NEW(x) operator new(x)
+#define ASSERT(exp)
+#endif  // NDEBUG
+
+// ベースサービスヘッダ
+#include "GameLib/Base/DebugStream.h"
+#include "GameLib/Base/Array.h"
+#include "GameLib/Base/MemoryManager.h"
+
+#endif
