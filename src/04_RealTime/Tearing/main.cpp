3611fd36c290053714dbd08350c47ec1cd2e4f02
neguse <negcee@gmail.com>
2025-03-10 18:42:19 +0900

    build 04

--- src/04_RealTime/Tearing/main.cpp
+++ src/04_RealTime/Tearing/main.cpp
@@ -1,13 +1,13 @@
-#include "GameLib/Framework.h"
+#include "Framework.h"
 
-namespace GameLib{
-	void Framework::update(){
-		unsigned* vram = videoMemory();
-		static int color = 1;
-		int pixelNumber = width() * height();
-		for ( int i = 0; i < pixelNumber; ++i ){
-			vram[ i ] = color;
-		}
-		color *= 3;
-	}
+namespace GameLib {
+void Framework::update() {
+  unsigned* vram = videoMemory();
+  static int color = 1;
+  int pixelNumber = width() * height();
+  for (int i = 0; i < pixelNumber; ++i) {
+    vram[i] = color;
+  }
+  color *= 3;
 }
+}  // namespace GameLib
