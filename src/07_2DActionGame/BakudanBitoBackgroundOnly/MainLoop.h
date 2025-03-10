752fd9a49b3a1e37732ebfd63644371c0b0b661a
neguse <negcee@gmail.com>
2025-03-11 00:40:00 +0900

    build 07

--- src/07_2DActionGame/BakudanBitoBackgroundOnly/MainLoop.h
+++ src/07_2DActionGame/BakudanBitoBackgroundOnly/MainLoop.h
@@ -1,49 +1,52 @@
 #ifndef INCLUDED_MAINLOOP_H
 #define INCLUDED_MAINLOOP_H
 
-namespace Sequence{
-	class TitleSequence;
-	class GameSequence;
-	class GameOverSequence;
-	class EndingSequence;
-}
+namespace Sequence {
+class Title;
+class GameOver;
+class Ending;
+namespace Game {
+class Parent;
+}  // namespace Game
+}  // namespace Sequence
 
-class MainLoop{
-public:
-	enum NextSequence{
-		NEXT_TITLE,
-		NEXT_GAME,
-		NEXT_GAME_OVER,
-		NEXT_ENDING,
+class MainLoop {
+ public:
+  enum NextSequence {
+    NEXT_TITLE,
+    NEXT_GAME,
+    NEXT_GAME_OVER,
+    NEXT_ENDING,
 
-		NEXT_NONE,
-	};
-	enum Mode{
-		MODE_1P,
-		MODE_2P,
-		MODE_NONE,
-	};
-	void update();
-	void moveTo( NextSequence );
-	Mode getMode() const;
-	void setMode( Mode );
+    NEXT_NONE,
+  };
+  enum Mode {
+    MODE_1P,
+    MODE_2P,
+    MODE_NONE,
+  };
+  void update();
+  void moveTo(NextSequence);
+  Mode getMode() const;
+  void setMode(Mode);
 
-	static void create();
-	static void destroy();
-	static MainLoop* getInstance();
-private:
-	MainLoop();
-	~MainLoop();
+  static void create();
+  static void destroy();
+  static MainLoop* getInstance();
 
-	NextSequence mNextSequence;
-	Mode mMode;
+ private:
+  MainLoop();
+  ~MainLoop();
 
-	Sequence::TitleSequence* mTitle;
-	Sequence::GameSequence* mGame;
-	Sequence::GameOverSequence* mGameOver;
-	Sequence::EndingSequence* mEnding;
+  NextSequence mNextSequence;
+  Mode mMode;
 
-	static MainLoop* mInstance;
+  Sequence::Title* mTitle;
+  Sequence::Game::Parent* mGame;
+  Sequence::GameOver* mGameOver;
+  Sequence::Ending* mEnding;
+
+  static MainLoop* mInstance;
 };
 
 #endif
