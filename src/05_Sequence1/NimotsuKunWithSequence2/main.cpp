c8c4913fc72d1b70cefa39523fde30799c427bfa
neguse <negcee@gmail.com>
2025-03-10 20:40:12 +0900

    build 05

--- src/05_Sequence1/NimotsuKunWithSequence2/main.cpp
+++ src/05_Sequence1/NimotsuKunWithSequence2/main.cpp
@@ -6,33 +6,32 @@ using namespace GameLib;
 #include "Image.h"
 #include "Sequence/Parent.h"
 
-//グローバル変数
-Sequence::Parent* gRootSequence = 0; //根っこシーケンス
-int gCounter = 0; //メインループを回った回数を数えるカウンタ
+// グローバル変数
+Sequence::Parent* gRootSequence = 0;  // 根っこシーケンス
+int gCounter = 0;  // メインループを回った回数を数えるカウンタ
 
-//ユーザ実装関数。中身はmainLoop()に丸投げ
-namespace GameLib{
-	void Framework::update(){
-		if ( !gRootSequence ){
-			gRootSequence = new Sequence::Parent();
-		}
-		//フレームレート調整
-		setFrameRate( 60 ); //一回呼べばいいのだが面倒なので呼んでしまう。
+// ユーザ実装関数。中身はmainLoop()に丸投げ
+namespace GameLib {
+void Framework::update() {
+  if (!gRootSequence) {
+    gRootSequence = new Sequence::Parent();
+  }
+  // フレームレート調整
+  setFrameRate(60);  // 一回呼べばいいのだが面倒なので呼んでしまう。
 
-		if ( gCounter % 60 == 0 ){ //60フレームに一回フレームレート表示
-			cout << " FrameRate:" << frameRate() << endl;
-		}
-		++gCounter;
+  if (gCounter % 60 == 0) {  // 60フレームに一回フレームレート表示
+    cout << " FrameRate:" << frameRate() << endl;
+  }
+  ++gCounter;
 
-		gRootSequence->update();
+  gRootSequence->update();
 
-		//終了判定(qが押されたか、マウスで×ボタンが叩かれたか)
-		if ( isKeyOn( 'q' ) ){
-			requestEnd();
-		}
-		if ( isEndRequested() ){
-			SAFE_DELETE( gRootSequence );
-		}
-	}
+  // 終了判定(qが押されたか、マウスで×ボタンが叩かれたか)
+  if (isKeyOn('q')) {
+    requestEnd();
+  }
+  if (isEndRequested()) {
+    SAFE_DELETE(gRootSequence);
+  }
 }
-
+}  // namespace GameLib
